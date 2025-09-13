85d94c7ff207b8f4492009133071f4d92f11df3e
chaitu <chaituved@gmail>
2025-09-13 15:00:40 +0530

    added string functio

--- app.py
+++ app.py
@@ -4,5 +4,8 @@ def add(a, b):
 def multiply(a, b):
     return a * b
 
+def add_str(s1,s2):
+    return s1 + s2
+
 if __name__ == "__main__":
     print(add(2, 3))
--- test_app.py
+++ test_app.py
@@ -1,7 +1,10 @@
-from app import add,multiply
+from app import add,multiply,add_str
 
 def test_add():
     assert add(2, 3) == 5
 
 def test_mul():
     assert multiply(2, 3) == 6
+
+def test_add_str():
+    assert add_str("Hello, ","World!") == "Hello, World!"
